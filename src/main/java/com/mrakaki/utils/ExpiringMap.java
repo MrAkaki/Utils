8cefe6679afded72c6300920cc7a77ff6909152a
Alex Vargas <vb.alexr@gmail.com>
2025-08-19 20:21:52 +0000

    feat: Make ExpiringMap generic

--- src/main/java/com/mrakaki/utils/ExpiringMap.java
+++ src/main/java/com/mrakaki/utils/ExpiringMap.java
@@ -1,24 +1,25 @@
 package com.mrakaki.utils;
 
+import java.io.Serializable;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-public class ExpiringMap {
-    private final ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>();
+public class ExpiringMap<K extends Serializable, V> {
+    private final ConcurrentHashMap<K, V> map = new ConcurrentHashMap<>();
     private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
-    public void put(String key, String value, long duration, TimeUnit unit) {
+    public void put(K key, V value, long duration, TimeUnit unit) {
         map.put(key, value);
         scheduler.schedule(() -> map.remove(key), duration, unit);
     }
 
-    public void remove(String key) {
+    public void remove(K key) {
         map.remove(key);
     }
 
-    public String get(String key) {
+    public V get(K key) {
         return map.get(key);
     }
 }
